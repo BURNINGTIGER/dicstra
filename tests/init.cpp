f64967c8a6860ee7f1bc9632e97819fa7e3afa58
BURNINGTIGER <Aleks203467@gmail.com>
2017-12-19 03:14:18 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -1,14 +1,6 @@
 #include <dicstra.hpp>
 #include <catch.hpp>
 
-
-SCENARIO("init", "[init]") {
-	dicstra init();
-	double hold;
-	hold=init.show()
-	REQUIRE(hold == nullptr);
-}
-
 SCENARIO("compute", "[compute]") {
 	dicstra init(std::ifstream("f1.txt"));
 	double hold;
