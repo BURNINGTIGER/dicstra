0941bdcae9478adbfaa30b3fe32c44183fc0c429
BURNINGTIGER <Aleks203467@gmail.com>
2017-12-19 03:31:05 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -2,7 +2,7 @@
 #include <catch.hpp>
 
 SCENARIO("compute", "[compute]") {
-	simplex init(std::ifstream("f1.txt"));
+	dicstra init(std::ifstream("f1.txt"));
 	double hold;
 	init.compute();
 	hold = init.show();
