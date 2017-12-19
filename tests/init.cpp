3fcbbdae0eec09aac879f30539b2441bcde6e41c
BURNINGTIGER <Aleks203467@gmail.com>
2017-12-19 04:12:59 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -1,16 +1,17 @@
 #include <dicstra.hpp>
 #include <catch.hpp>
-
+#include <iostream>
+#include <fstream>
 
 SCENARIO("compute", "[compute]") {
 	std::ofstream fout("f2.txt");
-	fout << "6";
-	fout << "0 7 9 0 0 14";
-	fout << "7 0 10 15 0 0";
-	fout << "9 10 0 11 0 2";
-	fout << "0 15 11 0 6 0";
-	fout << "0 0 0 6 0 9";
-	fout << "14 0 2 0 9 0";
+	fout << "6" << std::endl;
+	fout << "0 7 9 0 0 14" << std::endl;
+	fout << "7 0 10 15 0 0" << std::endl;
+	fout << "9 10 0 11 0 2" << std::endl;
+	fout << "0 15 11 0 6 0" << std::endl;
+	fout << "0 0 0 6 0 9" << std::endl;
+	fout << "14 0 2 0 9 0" << std::endl;
 	fout.close();
 	dicstra init(std::ifstream("f2.txt"));
 	double hold;
